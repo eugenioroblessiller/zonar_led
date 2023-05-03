4fbdf9520f7cdbf106e3d8db219122f402bee3cc
conti.eugenio <conti.eugenio@zonarsystems.com>
2023-05-03 17:05:04 -0600

    flutter zonar led

--- lib/bluetooth_search.dart
+++ lib/bluetooth_search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_bluetooth/flutter_web_bluetooth.dart';
 import 'package:provider/provider.dart';
 import 'package:zonar_led/main.dart';
 
@@ -8,48 +9,60 @@ class BluetoothSearch extends StatefulWidget {
 }
 
 class _BluetoothSearchState extends State<BluetoothSearch> {
+  final List<BluetoothDevice> _devices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _searchForDevices();
+  }
+
+  Future<void> _searchForDevices() async {
+    try {
+      final adapter = FlutterWebBluetooth.instance;
+      if (!adapter.isBluetoothApiSupported) {
+        print('Web Bluetooth API is not supported.');
+        return;
+      }
+
+      final device = await adapter.requestDevice(RequestOptionsBuilder.acceptAllDevices());
+
+      if (device != null) {
+        print('Found device: ${device.name} (${device.id})');
+        setState(() {
+          _devices.add(device);
+        });
+      }
+    } catch (e) {
+      print('Error searching for devices: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No devices yet.'),
-      );
-    }
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: const EdgeInsets.all(30),
-          child: Text('You have '
-              '${appState.favorites.length} devices:'),
+          child: Text('You have devices:'),
         ),
         Expanded(
-          // Make better use of wide windows with a grid.
-          child: GridView(
-            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 400,
-              childAspectRatio: 400 / 80,
-            ),
-            children: [
-              for (var pair in appState.favorites)
-                ListTile(
-                  leading: IconButton(
-                    icon: Icon(Icons.delete_outline, semanticLabel: 'Delete'),
-                    color: theme.colorScheme.primary,
-                    onPressed: () {
-                      appState.removeFavorite(pair);
-                    },
-                  ),
-                  title: Text(
-                    pair.asLowerCase,
-                    semanticsLabel: pair.asPascalCase,
-                  ),
-                ),
-            ],
+          child: ListView.builder(
+            itemCount: _devices.length,
+            itemBuilder: (context, index) {
+              final device = _devices[index];
+              return ListTile(
+                title: Text(device.name ?? ''),
+                subtitle: Text(device.id),
+                onTap: () {
+                  // TODO: connect to the selected device
+                },
+              );
+            },
           ),
         ),
       ],
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 import 'bluetooth_search.dart';
 import 'dynamic_grid.dart';
 
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -30,38 +29,10 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-  var history = <WordPair>[];
 
   int rows = 20;
   int cols = 20;
 
-  GlobalKey? historyListKey;
-
-  void getNext() {
-    history.insert(0, current);
-    var animatedList = historyListKey?.currentState as AnimatedListState?;
-    animatedList?.insertItem(0);
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  var favorites = <WordPair>[];
-
-  void toggleFavorite([WordPair? pair]) {
-    pair = pair ?? current;
-    if (favorites.contains(pair)) {
-      favorites.remove(pair);
-    } else {
-      favorites.add(pair);
-    }
-    notifyListeners();
-  }
-
-  void removeFavorite(WordPair pair) {
-    favorites.remove(pair);
-    notifyListeners();
-  }
 }
 
 class MyHomePage extends StatefulWidget {
