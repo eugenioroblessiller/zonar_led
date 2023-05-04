f11f9ee5a0ff85a97c8b8907daa960ff26bd05f5
conti.eugenio <conti.eugenio@zonarsystems.com>
2023-05-04 12:37:17 -0600

    functions to send

--- lib/connection_detail.dart
+++ lib/connection_detail.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
@@ -9,9 +10,39 @@ class ConnectionDetailsPage extends StatelessWidget {
   const ConnectionDetailsPage({Key? key, required this.device})
       : super(key: key);
 
+  void _sendPatternToBluetoothDevice() async {
+    // Set display pattern
+    // MessageType = 04
+    // FrameHeight = 1 byte
+    // FrameCount = 1 byte
+    // Pattern 3 bytes * frameHeight * frameCount = R,G,B (1 byte each)
+    const message = [
+      0xD0, // Start byte
+      0x04, // MessageType
+      0x03, // FrameHeight
+      0x03, // FrameCount
+      0x00, 0x00, 0xFF, // First frame (blue)
+      0x00, 0x00, 0xFF, // Second frame (blue)
+      0x00, 0x00, 0xFF, // Third frame (blue)
+      0xD1, // End byte
+    ];
+
+    // Find the characteristic to write to
+    final service = await device.discoverServices();
+    final characteristic = service
+        .map((s) => s.characteristics)
+        .expand((c) => c)
+        .firstWhere(
+            (c) => c.uuid == Guid('00002a28-0000-1000-8000-00805f9b34fb'));
+
+    // Write the message to the characteristic
+    final bytes = Uint8List.fromList(message);
+    await characteristic.write(bytes);
+  }
+
   void _sendStringToBluetoothDevice() async {
     // Generate a random string to send to the device
-    const randomString = 'Zonar';
+    const randomString = 'asdf';
 
     // Find the characteristic to write to
     final service = await device.discoverServices();
